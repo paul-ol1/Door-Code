4704b149074125599d93704f5e4c7942159b8f77
paul-ol1 <oladelepaul3@gmail.com>
2023-02-04 16:10:28 +0100

    4th feb

---
+++ script.js
@@ -0,0 +1,45 @@
+const code = "7654";
+let answer = "";
+count =0;
+const door = document.getElementById('dr');
+let buttons= document.getElementsByClassName('butt');
+
+
+function takecode(input){
+    const num = input.textContent;
+    if(count<4){
+        answer+= num;
+        count++;
+    }
+    if(answer=== code & count==4){
+        door.src = "open.jpg";
+        for(let x =0; x<buttons.length;x++){
+            buttons[x].style.backgroundColor="lightgreen";
+        }
+        
+    }
+    if(answer!= code & count==4){
+        count=0;
+        answer="";        
+    }
+    console.log(answer)
+    
+}
+function closedoor(){
+    count=0;
+    answer="";
+    door.src = "closed.jpg";
+    for(let x =0; x<buttons.length;x++){
+            buttons[x].style.backgroundColor="";
+        }
+}
+
+function deletecode(){
+    if(answer.length>0){
+        answer.slice(0, -1)
+        count--;
+    }
+    if(count==4){
+        alert("door is open")
+    }
+}
